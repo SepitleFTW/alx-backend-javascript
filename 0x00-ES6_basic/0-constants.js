375d917e038a672c1ef4df46aff84a25ef0987f2
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-06-24 20:25:53 +0200

    es6 basics

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,13 +3,13 @@ export function taskFirst() {
     return task;
   }
 
-  export function getLast() {
+  export function taskLast() {
     return ' is okay';
   }
 
   export function taskNext() {
     let combination = 'But sometimes let';
-    combination += getLast();
+    combination += taskLast();
 
     return combination;
   }
