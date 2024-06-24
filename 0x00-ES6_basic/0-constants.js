1cc8e7c7e773208192444be601fde592d271243e
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-06-24 18:27:08 +0000

    Mon, Jun 24, 2024, 8:27 PM +02:00

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -3,13 +3,13 @@ export function taskFirst() {
     return task;
   }
 
-  export function taskLast() {
+  export function getLast() {
     return ' is okay';
   }
 
   export function taskNext() {
     let combination = 'But sometimes let';
-    combination += taskLast();
+    combination += getLast();
 
     return combination;
   }
