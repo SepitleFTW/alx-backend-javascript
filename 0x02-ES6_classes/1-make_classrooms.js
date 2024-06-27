115a9a224a82526ec4440d9e3f0dc281dfe3fc14
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-06-27 22:04:32 +0200

    ...

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,11 @@
-import ClassRoom from './0-classroom';
+import Classroom from './0-classroom';
 
-export default function initializeRooms() {
-  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-  return rooms;
+function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
 }
+
+export default initializeRooms;
