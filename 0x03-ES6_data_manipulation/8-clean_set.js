0b1a0ec57e82e0146a2f1941cb5d814a9ce539b2
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-07-03 22:06:32 +0200

    everything rewcheckde

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,20 @@
 export default function cleanSet(set, startString) {
     if (
-        !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+      !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
     ) {
-        return '';
+      return '';
     }
 
     const parts = [];
 
     for (const value of set.values()) {
-        if (typeof vallue === 'string' && value.startsWith(startString)) {
-            const valueSubStr = value.substring(startString.length);
+      if (typeof value === 'string' && value.startsWith(startString)) {
+        const valueSubStr = value.substring(startString.length);
 
-            if (valueSubStr && valueSubStr !== value) {
-                parts.push(valueSubStr);
-            }
+        if (valueSubStr && valueSubStr !== value) {
+          parts.push(valueSubStr);
         }
+      }
     }
     return parts.join('-');
-}
+  }
